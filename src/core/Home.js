9e01d1848a8129f11b4023d9f217f66af3c3b8ff
ajpierskalla3 <ajpierskalla3@gmail.com>
2020-08-17 13:01:09 -0500

    changes made

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,64 +1,59 @@
-import React, { useState, useEffect } from 'react';
-import Layout from './Layout';
-import { getProducts } from './apiCore';
-import Card from './Card';
-import styled from 'styled-components'
+import React, { useState, useEffect } from "react";
+import Layout from "./Layout";
+import { getProducts } from "./apiCore";
+import Card from "./Card";
+import styled from "styled-components";
 // import CheckBox from './Checkbox';
 
-
-
-
-
 const Home = () => {
-    const [productsBySell, setProductsBySell] = useState([]);
-    const [productsByArrival, setProductsByArrival] = useState([]);
-    const [error, setError] = useState(false);
+  const [productsBySell, setProductsBySell] = useState([]);
+  const [productsByArrival, setProductsByArrival] = useState([]);
+  const [error, setError] = useState(false);
 
-    const loadProductsBySell = () => {
-        getProducts('sold').then(data => {
-            if (data.error) {
-                setError(data.error);
-            } else {
-                setProductsBySell(data);
-            }
-        });
-    };
+  const loadProductsBySell = () => {
+    getProducts("sold").then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProductsBySell(data);
+      }
+    });
+  };
 
-    const loadProductsByArrival = () => {
-        getProducts('createdAt').then(data => {
-            console.log(data);
-            if (data.error) {
-                setError(data.error);
-            } else {
-                setProductsByArrival(data);
-            }
-        });
-    };
+  const loadProductsByArrival = () => {
+    getProducts("createdAt").then((data) => {
+      console.log(data);
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProductsByArrival(data);
+      }
+    });
+  };
 
-    useEffect(() => {
-        loadProductsByArrival();
-        loadProductsBySell();
-    }, []);
+  useEffect(() => {
+    loadProductsByArrival();
+    loadProductsBySell();
+  }, []);
 
-    return (
-        <Layout
-            title="Welcome to Winos"
-            description="A place to save and rate all your favorite wines"
-            className="container-fluid"
-        >
-
-            {/* <Checkbox /> */}
-            <h2 className="mb-4">My Wines</h2>
-            <homeWrapper>
-                <div style={{ display: "unset" }} className="row">
-                    {productsByArrival.map((product, i) => (
-                        <div key={i} className="mb-3">
-                            <Card product={product} />
-                        </div>
-                    ))}
-                </div>
-            </homeWrapper>
-            {/* <h2 className="mb-4">Best Sellers</h2>
+  return (
+    <Layout
+      title="Welcome to Winos"
+      description="A place to save and rate all your favorite wines"
+      className="container-fluid"
+    >
+      {/* <Checkbox /> */}
+      <h2 className="mb-4">My Wines</h2>
+      <homeWrapper>
+        <div style={{ display: "unset" }} className="row">
+          {productsByArrival.map((product, i) => (
+            <div key={i} className="mb-3">
+              <Card product={product} />
+            </div>
+          ))}
+        </div>
+      </homeWrapper>
+      {/* <h2 className="mb-4">Best Sellers</h2>
             <div className="row">
                 {productsBySell.map((product, i) => (
                     <div key={i} className="col-4 mb-3">
@@ -66,8 +61,8 @@ const Home = () => {
                     </div>
                 ))}
             </div> */}
-        </Layout >
-    );
+    </Layout>
+  );
 };
 
 export default Home;
